e69e90931930fa19e1f8f5e030acbac70e9b3b88
MihaiGhe <mihai.md16@gmail.com>
2020-11-27 10:52:58 +0200

    Timer, responsive layout, buttons

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const questionImage = document.getElementById("img-question");
 const answersImage = document.getElementById("img-answers");
 const currentQuestion = document.getElementById("current-question");
-const button = document.getElementById("next");
-const scoreElement = document.getElementById("score");
+const nextButton = document.getElementById("next");
+const backButton = document.getElementById("back");
 
+// const scoreElement = document.getElementById("score");
+const time = document.querySelector("[data-label]");
+const progressBar = document.getElementsByClassName("progress-bar")[0];
 
 const CORRECT_ANSWERS = [
   undefined,
@@ -35,9 +38,11 @@ const CORRECT_ANSWERS = [
 
 let questionNumber = 1;
 let score = 0;
+timeLeft = 480;
+
 loadQuestion();
 
-button.addEventListener("click", function () {
+nextButton.addEventListener("click", function () {
   if (questionNumber < 24) {
     getAnswer();
     questionNumber++;
@@ -45,11 +50,30 @@ button.addEventListener("click", function () {
   }
 });
 
+backButton.addEventListener("click", function () {
+  if (questionNumber > 1) {
+    getAnswer();
+    questionNumber--
+    loadQuestion();
+  }
+});
+
+
+
+
+function loadQuestion() {
+  questionImage.src = `/img/question${questionNumber}.png`;
+  answersImage.src = `/img/answers${questionNumber}.png`;
+  currentQuestion.innerText =
+    questionNumber < 10 ? `0${questionNumber}` : questionNumber;
+  // scoreElement.innerText = score;
+}
+
 function checkAnswer(id) {
   console.log(questionNumber);
-  if (id == CORRECT_ANSWERS[questionNumber]) {
-    score++;
-  }
+  // if (id == CORRECT_ANSWERS[questionNumber]) {
+  //   score++;
+  // }
 }
 
 function getAnswer() {
@@ -66,22 +90,29 @@ function getAnswer() {
   });
 }
 
-function loadQuestion() {
-  questionImage.src = `/img/question${questionNumber}.png`;
-  answersImage.src = `/img/answers${questionNumber}.png`;
-  currentQuestion.innerText = questionNumber;
-  scoreElement.innerText = score;
-}
-
-const progressBar = document.getElementsByClassName("progress-bar")[0];
-
-const goat = setInterval(() => {
+const myInterval = setInterval(() => {
   const computedStyle = getComputedStyle(progressBar);
-  const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+  const width = parseFloat(computedStyle.getPropertyValue("--width"));
   progressBar.style.setProperty("--width", width - 0.1);
-  console.log(width);
-  if (width < 0.1) {
-    clearInterval(goat);
-  }
-}, 5);
 
+  if (width < 0.1) {
+    clearInterval(myInterval);
+  }
+}, 500);
+
+const timeInterval = setInterval(() => {
+  timeLeft = timeLeft - 1;
+
+  minute = Math.floor(timeLeft / 60);
+  second = Math.floor(timeLeft % 60);
+
+  if (second < 10) {
+    second = `0${second}`;
+  }
+
+  time.attributes[2].nodeValue = `0${minute}:${second}`;
+
+  if (timeLeft < 1) {
+    clearInterval(timeInterval);
+  }
+}, 1000);
