90eb1e700ba0234463675a08c9cb19aac1cfda04
MihaiGhe <mihai.md16@gmail.com>
2020-12-04 16:19:47 +0200

    Deploy

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const nextButton = document.getElementById("next");
 const backButton = document.getElementById("back");
 const restartButton = document.getElementById("restart");
 const finishButton = document.getElementById("finish");
+const startButton = document.getElementById("start");
+const radio = document.getElementById("radio");
+const scoreElement = document.getElementById('score')
 
-// const scoreElement = document.getElementById("score");
 const time = document.querySelector("[data-label]");
 const progressBar = document.getElementsByClassName("progress-bar")[0];
 
@@ -69,8 +71,21 @@ let selected_answers = [
 let questionNumber = 1;
 let score = 0;
 let timeLeft = 480;
+let isDone = false;
 
 loadQuestion();
+
+startButton.addEventListener("click", function () {
+  startIntervals();
+  document.querySelector(".modal-container").classList.remove("show");
+});
+
+restartButton.addEventListener("click", function () {
+  restart();
+  loadQuestion();
+});
+
+finishButton.addEventListener("click", finish);
 
 nextButton.addEventListener("click", function () {
   if (questionNumber < 24) {
@@ -88,31 +103,82 @@ backButton.addEventListener("click", function () {
   }
 });
 
-function loadQuestion() {
-  // scoreElement.innerText = score;
+function finish() {
+  getAnswer();
 
+  for (it = 1; it < 25; it++) {
+    if (CORRECT_ANSWERS[it] == selected_answers[it]) {
+      score++;
+    }
+  }
+
+  isDone = true;
+  loadQuestion();
+  radio.classList.add("none");
+  finishButton.removeEventListener("click", finish);
+  scoreElement.innerText = `You correctly answered ${score} questions`
+}
+
+function restart() {
+  score = 0;
+  timeLeft = 480;
+  questionNumber = 1;
+  isDone = false;
+
+  startIntervals();
+  finishButton.addEventListener("click", finish);
+
+  for (it = 1; it < selected_answers.length; it++) {
+    selected_answers[it] = undefined;
+  }
+
+  answers = document.querySelectorAll(".answer");
+  labels = document.querySelectorAll("#mylabel");
+
+  for (it = 0; it < 5; it++) {
+    answers[it].checked = false;
+    labels[it].classList.remove("white");
+    labels[it].classList.remove("correct");
+
+  }
+
+  time.attributes[2].nodeValue = `07:59`;
+  progressBar.style.setProperty("--width", 100);
+}
+
+function loadQuestion() {
   questionImage.src = `/img/question${questionNumber}.png`;
   answersImage.src = `/img/answers${questionNumber}.png`;
   currentQuestion.innerText =
     questionNumber < 10 ? `0${questionNumber}` : questionNumber;
 
   submited = selected_answers[questionNumber];
+
   if (submited != undefined) {
     answers = document.querySelectorAll(".answer");
     answers[submited].checked = true;
   }
-}
 
-function checkAnswer(id) {
-  if (id == CORRECT_ANSWERS[questionNumber]) {
-    console.log("You are correct");
-    console.log(`Question: ${questionNumber}`);
-    console.log(`Answer: ${id}`);
-    // score++;
-  } else {
-    console.log("You are wrong");
-    console.log(`Question: ${questionNumber}`);
-    console.log(`Answer: ${id}`);
+  if (isDone == true) {
+    answers = document.querySelectorAll("#mylabel");
+    correct = CORRECT_ANSWERS[questionNumber];
+
+    for (it = 0; it < 5; it++) {
+      el = answers[it];
+      if (it == correct) {
+        el.classList.remove("white");
+        el.classList.remove("black");
+        el.classList.add("correct");
+      } else if (it == submited) {
+        el.classList.remove("correct");
+        el.classList.remove("white");
+        el.classList.add('black')
+      } else {
+        el.classList.remove("correct");
+        el.classList.remove("black");
+        el.classList.add("white");
+      }
+    }
   }
 }
 
@@ -121,40 +187,44 @@ function getAnswer() {
 
   answers.forEach((answer) => {
     if (answer.checked) {
-      selected_answers[questionNumber] = answer.id
-      checkAnswer(answer.id);
+      selected_answers[questionNumber] = answer.id;
     }
   });
 
-  // CAUTION
   answers.forEach((answer) => {
     answer.checked = false;
   });
 }
 
-const myInterval = setInterval(() => {
-  const computedStyle = getComputedStyle(progressBar);
-  const width = parseFloat(computedStyle.getPropertyValue("--width"));
-  progressBar.style.setProperty("--width", width - 0.1);
+function startIntervals() {
+  animationInterval = setInterval(() => {
+    const computedStyle = getComputedStyle(progressBar);
+    const width = parseFloat(computedStyle.getPropertyValue("--width"));
+    progressBar.style.setProperty("--width", width - 0.1);
 
-  if (width < 0.1) {
-    clearInterval(myInterval);
-  }
-}, 500);
+    if (width < 0.1 || isDone == 1) {
+      clearInterval(animationInterval);
+    }
+  }, 500);
 
-const timeInterval = setInterval(() => {
-  timeLeft = timeLeft - 1;
+  timeInterval = setInterval(() => {
+    timeLeft = timeLeft - 1;
 
-  minute = Math.floor(timeLeft / 60);
-  second = Math.floor(timeLeft % 60);
+    minute = Math.floor(timeLeft / 60);
+    second = Math.floor(timeLeft % 60);
 
-  if (second < 10) {
-    second = `0${second}`;
-  }
+    if (second < 10) {
+      second = `0${second}`;
+    }
 
-  time.attributes[2].nodeValue = `0${minute}:${second}`;
+    console.log(minute);
+    console.log(second);
+    time.attributes[2].nodeValue = `0${minute}:${second}`;
 
-  if (timeLeft < 1) {
-    clearInterval(timeInterval);
-  }
-}, 1000);
+    if (timeLeft < 1 || isDone == 1) {
+      clearInterval(timeInterval);
+      clearInterval(animationInterval);
+      finish()
+    }
+  }, 1000);
+}
